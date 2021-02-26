572e6a5c0b94d3b098d0a6026635cf41b3109efb
Andii Magyar <andiimagyar@Andiis-MacBook-Pro.local>
2021-02-25 22:57:42 -0700

    draggable to trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,9 +157,73 @@ $("#task-form-modal .btn-primary").click(function() {
       date: taskDate
     });
 
-    saveTasks();
+
+  saveTasks();
   }
 });
+
+$(".card .list-group").sortable({
+  connectWith: $('.card .list-group'),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event){
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function() {
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function() {
+        // save values in temp array
+        tempArr.push({
+          text: $(this)
+            .find("p")
+            .text()
+            .trim(),
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
+        });
+      });
+
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+    
+    }
+    });
+
+    $("#trash").droppable({
+      accept: ".card .list-group-item",
+      tolerance: "touch",
+      drop: function(event, ui){
+        ui.draggable.remove();
+        console.log("drop");
+      },
+      over: function(event, ui) {
+        console.log("over");
+      },
+      out: function(event, ui) {
+        console.log("out");
+      }
+    });
+
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
